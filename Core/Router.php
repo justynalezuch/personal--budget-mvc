4a5d196ffc71fee5e7d39c43c070a27e78956b78
justynalezuch <justynalezuch@gmail.com>
2020-11-02 15:34:37 +0100

    Get controller and action from url with a variable structure

--- Core/Router.php
+++ Core/Router.php
@@ -28,8 +28,17 @@ class Router
      *
      * @return void
      */
-    public function add($route, $params)
+    public function add($route, $params = [])
     {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route] = $params;
     }
 
@@ -53,19 +62,18 @@ class Router
      */
     public function match($url)
     {
-        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
-
-        if(preg_match($reg_exp, $url, $matches)) {
-            foreach ($matches as $key => $match) {
-                if(is_string($key)) {
-                    $params[$key] = $match;
+        foreach($this->routes as $route => $params) {
+            if(preg_match($route, $url, $matches)) {
+                foreach ($matches as $key => $match) {
+                    if(is_string($key)) {
+                        $params[$key] = $match;
+                    }
                 }
+                $this->params = $params;
+                return true;
             }
-            $this->params = $params;
-            return true;
         }
         return false;
-
     }
 
     /**
