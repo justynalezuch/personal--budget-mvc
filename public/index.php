e2892b34ca11ffa3ba1551604f7ddccc34fea91d
justynalezuch <justynalezuch@gmail.com>
2020-11-02 15:50:34 +0100

    Adding custom variables to url

--- public/index.php
+++ public/index.php
@@ -19,6 +19,7 @@ $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 //$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{teat}');
+$router->add('{controller}/{id:\d+}/{teat}');
 // Display the routing table
 echo '<pre>';
 //var_dump($router->getRoutes());
