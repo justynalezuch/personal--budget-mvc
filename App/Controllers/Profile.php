455fe3cb962816f3902bb973cf5f5638ba6d61f9
justynalezuch <justynalezuch@gmail.com>
2020-12-31 12:04:39 +0100

    Display user data

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -5,12 +5,15 @@ namespace App\Controllers;
 
 
 use Core\View;
+use App\Auth;
 
 class Profile extends Authenticated
 {
     public function showAction() {
 
-        View::renderTemplate('Profile/show.html');
+        View::renderTemplate('Profile/show.html', [
+            'user' => Auth::getUser()
+        ]);
 
     }
 
